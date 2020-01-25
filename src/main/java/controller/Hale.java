413bd5e6ec15cccc1f7b3148663e13f196e284bf
Krzysztof Sychla <ksychla5@gmail.com>
2020-01-25 18:50:05 +0100

    done: adding everything

--- src/main/java/controller/Hale.java
+++ src/main/java/controller/Hale.java
@@ -1,6 +1,7 @@
 package main.java.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import main.java.base.DataBase;
 
 public class Hale extends MainView {
@@ -14,5 +15,12 @@ public class Hale extends MainView {
     @FXML
     private void initialize(){
         title.setText("Hale konferencyjne");
+        Label numer = new Label("Numer");
+        Label liczbaOsob = new Label("Liczba osób");
+        numer.setStyle("-fx-padding: 0 300 0 0;");
+        numer.getStyleClass().add("tag");
+        liczbaOsob.setStyle("-fx-padding: 0 50 0 0;");
+        liczbaOsob.getStyleClass().add("tag");
+        tagsHBox.getChildren().addAll(numer, liczbaOsob);
     }
 }
--- src/main/java/controller/Pokoje.java
+++ src/main/java/controller/Pokoje.java
@@ -1,6 +1,7 @@
 package main.java.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import main.java.base.DataBase;
 
 public class Pokoje extends MainView {
@@ -14,5 +15,21 @@ public class Pokoje extends MainView {
     @FXML
     private void initialize(){
         title.setText("Pokoje");
+        Label numer = new Label("Numer");
+        Label cena = new Label("Cena");
+        Label liczbaLozek = new Label("Liczba łóżek");
+        Label telewizor = new Label("Telewizor");
+        Label lazienka = new Label("Łazienka");
+        numer.setStyle("-fx-padding: 0 50 0 0;");
+        numer.getStyleClass().add("tag");
+        cena.setStyle("-fx-padding: 0 150 0 0;");
+        cena.getStyleClass().add("tag");
+        liczbaLozek.setStyle("-fx-padding: 0 200 0 0;");
+        liczbaLozek.getStyleClass().add("tag");
+        telewizor.setStyle("-fx-padding: 0 50 0 0;");
+        telewizor.getStyleClass().add("tag");
+        lazienka.setStyle("-fx-padding: 0 0 0 0;");
+        lazienka.getStyleClass().add("tag");
+        tagsHBox.getChildren().addAll(numer, cena, liczbaLozek, telewizor, lazienka);
     }
 }
